33d46941acdddd7c5c0abf2037e5fda2f75d0777
ChourasiaAshish1996 <ashish.org007@gmail.com>
2023-02-24 17:58:49 +0530

    2) Vcs.xml added

--- app/src/main/java/com/example/newemployee/Data/EmployeeListRepo.kt
+++ app/src/main/java/com/example/newemployee/Data/EmployeeListRepo.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.LiveData
 
 
 class EmployeeListRepo(context : Application) {
-    private val employeeListDao : EmployeeListDao = EmployeeDatabase.getDatabase(context).EmployeeListDao
+    private val employeeListDao : EmployeeListDao = EmployeeDatabase.getDatabase(context).employeeListDao()
 
     fun getEmployees (): LiveData<List<Employee>> = employeeListDao.getEmployee()
 }
